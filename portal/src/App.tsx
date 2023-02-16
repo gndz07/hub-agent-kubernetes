7177ad596f60d262213d41f504603984f18a0611
gndz07 <ginadzani@gmail.com>
2023-02-16 15:17:16 +0100

    fix: apply theme color

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FaencyProvider, globalCss } from '@traefiklabs/faency'
+import React, { useEffect } from 'react'
+import { FaencyProvider, globalCss, lightTheme } from '@traefiklabs/faency'
 import PageLayout from 'components/PageLayout'
 import { BrowserRouter, Route, Routes as RouterRoutes } from 'react-router-dom'
 import Dashboard from 'components/Dashboard'
@@ -9,6 +9,8 @@ import { HelmetProvider } from 'react-helmet-async'
 import { QueryClientProvider, QueryClient } from 'react-query'
 
 const queryClient = new QueryClient()
+
+const light = lightTheme('blue')
 
 const bodyGlobalStyle = globalCss({
   body: {
@@ -29,18 +31,24 @@ const Routes = () => {
   )
 }
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <HelmetProvider>
-      <FaencyProvider>
-        <BrowserRouter>
-          <PageLayout catalogName={catalogName}>
-            <Routes />
-          </PageLayout>
-        </BrowserRouter>
-      </FaencyProvider>
-    </HelmetProvider>
-  </QueryClientProvider>
-)
+const App = () => {
+  useEffect(() => {
+    document.body.classList.add(light.toString())
+  }, [])
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <FaencyProvider>
+          <BrowserRouter>
+            <PageLayout catalogName={catalogName}>
+              <Routes />
+            </PageLayout>
+          </BrowserRouter>
+        </FaencyProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
+  )
+}
 
 export default App
